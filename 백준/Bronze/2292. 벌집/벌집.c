64dfa9c8ea7eb362397241a7af58ab2a7b61e5d2
kmbae <102423525+kmbae1142@users.noreply.github.com>
2024-12-30 14:38:32 +0900

    [Bronze II] Title: 벌집, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.c
+++ 백준/Bronze/2292. 벌집/벌집.c
@@ -5,13 +5,12 @@ int main() {
 	int N, start = 1, end = 1, i = 1;
 	scanf("%d", &N);
 
-	while (1) {
+	while (!(start <= N && N <= end)) {
 		if (N == 1) { break; }
 		if (i == 1) start += 1;
 		else start += 6 * (i - 1);
 		end += 6 * i;
 		i++;
-		if (start <= N && N <= end) break;
 	}
 	
 	printf("%d\n", i);
