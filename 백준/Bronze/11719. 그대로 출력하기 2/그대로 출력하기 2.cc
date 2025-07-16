1fb937147cd952f45eeda9c770e76b7a9a978f61
kmbae <102423525+kmbae1142@users.noreply.github.com>
2025-07-17 00:03:54 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	string str;
+
+	while (getline(cin, str)) {
+		cout << str << "\n";
+	}
+
+	return 0;
+
+}
