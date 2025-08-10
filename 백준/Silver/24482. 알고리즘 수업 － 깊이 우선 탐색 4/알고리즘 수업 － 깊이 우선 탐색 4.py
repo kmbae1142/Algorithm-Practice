296e66871bfb128c3e9def177ac06a7ad99d503f
kmbae <102423525+kmbae1142@users.noreply.github.com>
2025-08-10 14:17:11 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 4, Time: 588 ms, Memory: 75816 KB -BaekjoonHub

---
+++ 백준/Silver/24482. 알고리즘 수업 － 깊이 우선 탐색 4/알고리즘 수업 － 깊이 우선 탐색 4.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+def dfs(graph, V, visited, dist):
+    visited[V] = True
+    graph[V].sort(reverse=True)
+    for i in graph[V]:
+        if not visited[i]:
+            dist[i] = dist[V] + 1
+            dfs(graph, i, visited, dist)
+
+N, M, R = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+visited = [False] * (N + 1)
+dist = [0] * (N + 1)
+
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+dfs(graph, R, visited, dist)
+for i, j in zip(visited[1:], dist[1:]):
+    if i:
+        print(j)
+    else:
+        print(-1)
