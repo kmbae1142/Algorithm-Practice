ed9b93b569f870d67cf9c787ab53628bf304d769
kmbae <102423525+kmbae1142@users.noreply.github.com>
2025-09-06 18:04:35 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 1120 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define max(a, b) ((a) > (b)) ? (a) : (b)
+
+int A[1001], dp[1001];
+
+int main() {
+
+    int N, max_len = 0;
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; i++) {
+        scanf("%d", &A[i]);
+        int cur = 0;
+        for (int j = 1; j < i; j++) {
+            if (A[i] > A[j])
+                cur = max(cur, dp[j]);
+        }
+        dp[i] = cur + 1;
+        max_len = max(max_len, dp[i]);
+    }
+
+    printf("%d", max_len);
+    return 0;
+
+}
