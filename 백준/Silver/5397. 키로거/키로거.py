110ac431a4dfc2b3eed893f057f32c4e288e0860
kmbae <102423525+kmbae1142@users.noreply.github.com>
2025-09-19 00:23:25 +0900

    [Silver II] Title: 키로거, Time: 1000 ms, Memory: 44508 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    str = input().strip()
+    left, right = deque([]), deque([])
+
+    for i in str:
+        if i == '<':
+            if left:
+                right.appendleft(left.pop())
+        elif i == '>':
+            if right:
+                left.append(right.popleft())
+        elif i == '-':
+            if left:
+                left.pop()
+        else:
+            left.append(i)
+
+    print(''.join(left) + ''.join(right))
