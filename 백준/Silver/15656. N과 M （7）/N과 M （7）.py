18d9d13e2479c30fa360d2f60a1a3371e2b8c176
kmbae <102423525+kmbae1142@users.noreply.github.com>
2025-09-20 15:22:40 +0900

    [Silver III] Title: N과 M (7), Time: 2016 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+def find(N, M, num: list, ans: list):
+    if len(ans) == M:
+        print(*ans)
+        return
+    for i in num:
+        ans.append(i)
+        find(N, M, num, ans)
+        ans.pop()
+
+N, M = map(int, input().split())
+num = list(map(int, input().split()))
+num.sort()
+find(N, M, num, [])
