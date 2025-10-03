5da73331d54209c7660870434be9ea7220cde8f7
kmbae <102423525+kmbae1142@users.noreply.github.com>
2025-10-03 15:18:46 +0900

    [Silver II] Title: 베르트랑 공준, Time: 8 ms, Memory: 2264 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#define LEN 246913
+
+using namespace std;
+
+int main() {
+
+	int n;
+	bool* primes = new bool[LEN];
+	fill(primes, primes + LEN, true);
+	primes[2] = true;
+
+	for (int i = 2; i * i < LEN; i++) {
+		if (!primes[i]) continue;
+		for (int j = i * i; j < LEN; j += i) {
+			primes[j] = false;
+		}
+	}
+
+	while (true) {
+		int cnt = 0;
+		cin >> n;
+		if (!n) break;
+
+		for (int i = n + 1; i <= 2 * n; ++i) {
+			if (primes[i]) cnt++;
+		}
+		cout << cnt << '\n';
+	}
+
+}
