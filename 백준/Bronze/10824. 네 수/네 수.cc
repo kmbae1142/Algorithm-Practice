d355ec1a673ce61c4ff7e8b714b4f28ff6b1f094
kmbae <102423525+kmbae1142@users.noreply.github.com>
2026-02-24 13:31:57 +0900

    [Bronze III] Title: 네 수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string a, b, c, d;
+	std::cin >> a >> b >> c >> d;
+
+	std::cout << std::stoll(a + b) + std::stoll(c + d);
+    return 0;
+}
