ca3cdc19f4074e5194b746e6e01156000a81833a
kmbae <102423525+kmbae1142@users.noreply.github.com>
2026-03-26 18:43:26 +0900

    [Gold IV] Title: 미적분학 입문하기 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Gold/24726. 미적분학 입문하기 2/미적분학 입문하기 2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	
+	int x1, y1, x2, y2, x3, y3;
+	scanf("%d %d %d %d %d %d", &x1, &y1, &x2, &y2, &x3, &y3);
+
+	double r_x = (double)(x1 + x2 + x3) / 3;
+	double r_y = (double)(y1 + y2 + y3) / 3;
+	double A = 0.5 * abs(x1 * y2 + x2 * y3 + x3 * y1 - x2 * y1 - x3 * y2 - x1 * y3);
+	double pi = 3.14159265358979;
+	
+	printf("%.9f %.9f", 2 * pi * r_y * A, 2 * pi * r_x * A);
+	return 0;
+
+}
