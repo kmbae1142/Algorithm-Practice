8fca60c6a4a87a72b33565f39329f73f71d6dd03
kmbae <102423525+kmbae1142@users.noreply.github.com>
2026-04-09 14:30:53 +0900

    [Bronze III] Title: 지능형 기차, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2455. 지능형 기차/지능형 기차.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int people = 0, max = -1;
+
+	for (int i = 0; i < 4; i++) {
+		int out, in;
+		scanf("%d %d", &out, &in);
+
+		people += in - out;
+		max = max < people ? people : max;
+	}
+
+	printf("%d", max);
+	return 0;
+}
